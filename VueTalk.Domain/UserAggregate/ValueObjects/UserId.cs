125ba9b9b221f2b5a344d97f742e44ea7d7ab2c0
Mike Ponton <pontonmikecgi@gmail.com>
2024-06-08 04:36:51 +0200

    [Authentication] Added Users Aggregate

---
+++ VueTalk.Domain/UserAggregate/ValueObjects/UserId.cs
@@ -0,0 +1,29 @@
+﻿using VueTalk.Domain.Common.Models;
+
+namespace VueTalk.Domain.UserAggregate.ValueObjects;
+
+public sealed class UserId : ValueObject
+{
+    private UserId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static UserId CreateUnique()
+    {
+        return new UserId(new Guid());
+    }
+
+    public static UserId Create(Guid userId)
+    {
+        return new UserId(userId);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+}
